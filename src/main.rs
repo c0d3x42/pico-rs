b3f1f8f8d18cac276a34b3d3fee45af8260514a3
Vince <vince@openanswers.co.uk>
2020-05-23 16:15:54 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 extern crate serde;
 extern crate valico;
 
-use serde_json::{Value,to_string_pretty};
+use serde_json::{Value};
 use valico::json_schema;
 use std::fs::File;
 
@@ -31,7 +31,7 @@ enum Var {
 
 
 #[derive(Serialize, Deserialize, Debug)]
-struct PicoConditionEqualityTuple (Var, Var, Var);
+struct PicoConditionEqualityTuple (Var, Var);
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PicoConditionEquality {
@@ -50,23 +50,101 @@ struct PicoConditionOr {
     or: Vec<PicoConditions>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug )]
 #[serde(untagged)]
-enum PicoConditions {
+enum PicoConditionsO {
 //    And { and: Vec<String> },
     And(PicoConditionAnd),
+//    PicoConditionAnd,
     Or(PicoConditionOr),
+//    PicoConditionOr,
 //    Simple(PicoConditionSimple)
+    Eq(PicoConditionEquality),
+//    PicoConditionEquality
+}
+
+#[derive(Serialize, Deserialize, Debug )]
+#[serde(untagged)]
+enum PicoConditions {
+    And(PicoConditionAnd),
+    Or(PicoConditionOr),
     Eq(PicoConditionEquality)
 }
 
+
+impl Executable for PicoConditions{
+    fn exec(&self) -> bool {
+        match &*self {
+            PicoConditions::And(_x) => { return _x.exec(); },
+            PicoConditions::Or(_x) => { return _x.exec();},
+            PicoConditions::Eq(_x) => { return _x.exec();},
+        }
+    }
+}
+
+
+trait Executable {
+    fn exec(&self)->bool;
+}
+
+impl Executable for PicoConditionAnd {
+    fn exec(&self)->bool{
+        println!("Condition AND {:?}", self);
+        let itterator = self.and.iter();
+        let mut counter = 0;
+        for c in itterator{
+            println!("Itterating [{:?}]", counter);
+            counter = counter+1;
+            c.exec();
+        }
+        let b = self.and.iter().map(|c|{ c.exec()}).collect::<Vec<bool>>();
+        println!("Condition AND ----- booleans {:?}", b);
+        return true;
+    }
+}
+impl Executable for PicoConditionOr {
+    fn exec(&self)->bool{
+        println!("Condition OR {:?}", self);
+
+        let b = self.or.iter().map(|c|{ c.exec()}).collect::<Vec<bool>>();
+        println!("Condition OR booleans {:?}", b);
+
+        for c in self.or.iter(){
+            c.exec();
+        }
+        return true;
+    }
+}
+
+impl Executable for PicoConditionEquality {
+    fn exec(&self)->bool{
+        println!("Condition EQ {:?}", self);
+        return true;
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PicoIfThenElseTuple (PicoConditions, String, String);
 
 #[derive(Serialize, Deserialize, Debug)]
 struct PicoIfThenElse {
-    r#if: PicoIfThenElseTuple
+    ite: (PicoConditions, String, String)
+}
+
+impl PicoIfThenElse {
+    pub fn exec(&self) -> bool {
+//        println!("Exec: {:?}", self.r#if.0);
+//        let c = &self.r#if;
+//        let i = &c.0;
+        let i = &self.ite.0;
+
+        i.exec();
+        
+        println!("Exec IIIIi 0: {:?}", i);
+        println!("Exec IIIIi 1: {:?}", self.ite.1);
+        println!("Exec IIIIi 2: {:?}", self.ite.2);
+        return true;
+    }
 }
 
 
@@ -81,6 +159,9 @@ fn main() {
     println!("Is valid: {}", schema.validate(&json_v4_schema).is_valid());
 
     let json_rules: PicoIfThenElse = serde_json::from_reader(File::open("pico.json").unwrap()).unwrap();
-    println!("Pico rules: {:?}", json_rules)
+    println!("Pico rules: {:?}", json_rules);
+
+    let truth = json_rules.exec();
+    println!("Truth: {:?}", truth);
 
 }
